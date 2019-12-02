6bd33779162ba6502299cae2bd649c6db40a27a6
Rokax1 <leandrofernando739@gmail.com>
2019-12-01 22:16:04 -0300

    seleccionar servicios dinamicos

--- app/Http/Controllers/seguridad/LoginController.php
+++ app/Http/Controllers/seguridad/LoginController.php
@@ -61,7 +61,7 @@ class LoginController extends Controller
             return '/barberos';
         }elseif(session()->get('tipo_usuario') == 3){
 
-            return '/Reservas';
+            return '/Reservas/CalendarioReservas';
         }
 
         
