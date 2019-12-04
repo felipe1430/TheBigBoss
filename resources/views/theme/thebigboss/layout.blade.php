5bc733f7d7e958070396663a72783f6663e5be89
Rokax1 <leandrofernando739@gmail.com>
2019-12-04 00:00:24 -0300

    Servicios final casii

--- resources/views/theme/thebigboss/layout.blade.php
+++ resources/views/theme/thebigboss/layout.blade.php
@@ -73,8 +73,8 @@
   <script src="{{asset("assets/$theme/js/bootstrap-datepicker.js")}}"></script>
   <script src="{{asset("assets/$theme/js/jquery.timepicker.min.js")}}"></script>
   <script src="{{asset("assets/$theme/js/scrollax.min.js")}}"></script>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-  <script src="{{asset("assets/$theme/js/google-map.js")}}"></script>
+  {{-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script> --}}
+  {{-- <script src="{{asset("assets/$theme/js/google-map.js")}}"></script> --}}
   <script src="{{asset("assets/$theme/js/main.js")}}"></script>
     @yield('script')
   </body>
