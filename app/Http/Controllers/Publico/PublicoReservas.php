b7f5fdfab966240e9fda9e54a37269cd26839aa8
Rokax1 <leandrofernando739@gmail.com>
2019-12-14 11:07:31 -0300

    resinicio de reserva

--- app/Http/Controllers/Publico/PublicoReservas.php
+++ app/Http/Controllers/Publico/PublicoReservas.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use App\empleados;
 use App\reservas;
 use App\servicios;
+use DB;
+use Carbon\Carbon;
 
 class PublicoReservas extends Controller
 {
@@ -55,21 +57,110 @@ class PublicoReservas extends Controller
        //dd( $request->all());
        if ($request->ajax()) {
            
-           dd($request->all());
 
-        $reserva = new reservas;
-        $reserva->title = $request->nameUser;
-        $reserva->color = '#1C669F';
-        $reserva->start_date = $request->start_date;
-        $reserva->end_date = $request->end_date;
-        $reserva->fk_id_usuario = $request->idUser;
-        $reserva->fk_id_empleado = $request->idBarbero;
-        $reserva->fecha_reserva = '2019-11-27';
-        $reserva->estado_reserva = 1;
-        $reserva->save();
+        $params_array   =$request->all();
+        
+        $nombreUser     =$params_array['nameUser'];
+        $idUser         =$params_array['idUser'];
+        $idBarbero      =$params_array['idBarbero'];
+        $start_date     =$params_array['start_date'];
+        $end_date       =$params_array['end_date'];
+
+        $date = Carbon::now();
+        $date = $date->format('Y-m-d');
+        $id=0;
+
+        try{
+           
+            DB::beginTransaction();
+
+                $id = DB::table('reservas')->insertGetId(
+                    ['title' => $nombreUser,
+                     'color' => '#1C669F',
+                     'start_date' => $start_date,
+                     'end_date' => $end_date,
+                     'fk_id_usuario' => $idUser,
+                     'fk_id_empleado' => $idBarbero,
+                     'fecha_reserva' => $date,
+                     'estado_reserva' => 1, ]
+                );
+               
+                /*
+                        $reserva->title = $nombreUser;
+                        $reserva->color = '#1C669F';
+                        $reserva->start_date =  $start_date;
+                        $reserva->end_date =  $end_date; 
+                        $reserva->fk_id_usuario =  $idUser;
+                        $reserva->fk_id_empleado = $idBarbero;  
+                        $reserva->fecha_reserva = $date;
+                        $reserva->estado_reserva = 1;
+                        //**/
+            DB::commit();
+
+        }catch(Exception $e){
+            DB::rollback();
+            dd($e,'1');
+        } catch (\Throwable $e) {
+            DB::rollback();
+            dd($e,'2');
+            throw $e;
+        }
+
+        //dd($id);
+       
+
+            
+                
+
+
+        unset($params_array['nameUser']);
+        unset($params_array['idUser']);
+        unset($params_array['idBarbero']);
+        unset($params_array['start_date']);
+        unset($params_array['start_date']);
+        unset($params_array['end_date']);
+        unset($params_array['_token']);
+
+        $cantidadServicios=count($params_array);
+        dd($params_array,$cantidadServicios);
+        if(!($id==0)){
+
+                try{
+
+                    DB::beginTransaction();
+
+                    for ($i = 1; $i <=$cantidadServicios ; $i++) {
+
+                        DB::table('detalle_reserva')->insert([
+                            'fk_reserva'=>$id,
+                            'fk_servicio'=>$params_array['servicio'],
+                            'cantidad_serv_det_reserva'=>1,
+                            ]);
+
+                    }
+                            
+                            
+
+                    DB::commit();
+                }catch(Exception $e){
+
+
+                }
+
+
+        }else{
+
+
+        }
+        
+
+
 
         
-       }
+        
+
+        
+       }// if ajax fin 
 
     }
 }
--- resources/views/seguridad/index.blade.php
+++ resources/views/seguridad/index.blade.php
@@ -115,13 +115,13 @@
                             </div>             
             </div>
             <div class="modal-footer">
-                    <div class="form-group row mb-">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Registrar') }}
-                                </button>
-                            </div>
-                        </div>
+                
+                            
+                      <button type="submit" class="btn btn-primary">
+                          {{ __('Registrar') }}
+                      </button>
+                  
+                        
             </form>
             </div>
           </div>
